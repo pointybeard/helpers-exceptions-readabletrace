e12e324e3aaa4951236a12376ece707d0180783f
Alannah Kearney <hi@alannahkearney.com>
2019-05-26 17:48:57 +1000

    Added $format argument to ReadableTraceException::getReadableTrace() which allows changing how each line of the trace looks. Trace is no longer appended to the exception message. This gives better control to how/when the trace gets displayed.

--- src/ReadableTrace/ReadableTraceException.php
+++ src/ReadableTrace/ReadableTraceException.php
@@ -5,29 +5,20 @@ declare(strict_types=1);
 namespace pointybeard\Helpers\Exceptions\ReadableTrace;
 
 use pointybeard\Helpers\Functions\Paths;
+use pointybeard\Helpers\Functions\Strings;
 
 class ReadableTraceException extends \Exception implements Interfaces\ReadableTraceExceptionInterface
 {
-    public function __construct(string $message, $code = 0, \Exception $previous = null)
-    {
-        return parent::__construct(sprintf(
-            "%s\r\n\r\nTrace\r\n==========\r\n%s\r\n",
-            $message,
-            $this->getReadableTrace()
-        ), $code, $previous);
-    }
-
-    public function getReadableTrace(): ?string
+    public function getReadableTrace(string $format = '[{{PATH}}/{{FILENAME}}:{{LINE}}] {{CLASS}}{{TYPE}}{{FUNCTION}}();'): ?string
     {
         // Nothing in the trace
         if (count($this->getTrace()) <= 0) {
             return null;
         }
 
-        $traceLineFormat = '[%s:%d] %s%s%s();';
-
-        $baseLine = [
-            'relative' => null,
+        $base = [
+            'path' => null,
+            'filename' => null,
             'line' => null,
             'class' => null,
             'type' => null,
@@ -36,27 +27,40 @@ class ReadableTraceException extends \Exception implements Interfaces\ReadableTr
             'args' => [],
         ];
 
+        // Set up the placeholder array (remove last 2 items, get the array keys
+        // and make each one upper case)
+        $placeholders = array_map('strtoupper', array_keys(array_slice($base, 0, count($base) - 2)));
+
         $lines = [];
 
         foreach ($this->getTrace() as $line) {
             if (null !== $line['file']) {
                 try {
-                    $line['relative'] = Paths\get_relative_path(getcwd(), $line['file']);
+                    $line['filename'] = basename($line['file']);
+                    $line['path'] = dirname(Paths\get_relative_path(getcwd(), $line['file']));
 
                     // Something when wrong. Just use the full file path instead
                 } catch (\Exception $ex) {
-                    $line['relative'] = $line['file'];
+                    $line['path'] = $line['file'];
                 }
             }
 
             // This will keep values from $line but order them according to
-            // $baseLine's array keys, otherwise the result from vsprintf will
+            // array keys of $base, otherwise the result from vsprintf() will
             // be wonky
-            $line = array_merge($baseLine, $line);
+            $line = array_merge($base, $line);
 
-            $lines[] = vsprintf(
-                $traceLineFormat,
-                array_slice($line, 0, 5) // We don't care about 'file' or 'args'
+            // Now remove the last two items (file and args) since we don't need
+            // to worry about them
+            $line = array_slice($line, 0, count($line) - 2);
+
+            // Replace the placeholder vaules in $format to produce the final
+            // line
+            $lines[] = Strings\replace_placeholders_in_string(
+                $placeholders,
+                array_values($line),
+                $format,
+                true
             );
         }
 
